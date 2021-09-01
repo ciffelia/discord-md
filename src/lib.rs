62a9d4671de10dabde1c59d45f374cd0d117df1e
Ciffelia <mc.prince.0203@gmail.com>
2021-09-01 23:19:58 +0900

    Add `parse` function that returns MarkdownDocument

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,14 @@
 pub mod ast;
 mod parser;
 
-pub use parser::markdown_document as parse;
+use crate::ast::MarkdownDocument;
+
+pub fn parse(msg: &str) -> MarkdownDocument {
+    // Since there are no invalid markdown document, parsing should never fails.
+    let (rest, doc) = parser::markdown_document(msg).unwrap();
+
+    // All input should be consumed.
+    assert!(rest.is_empty());
+
+    doc
+}
